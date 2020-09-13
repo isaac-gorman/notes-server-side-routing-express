dde2bef6f0950f83f3d9bb6069ebc81b88adda2b
Isaac Gorman <emailme@isaacg.net>
2020-09-13 17:10:53 -0500

    pre module material annotated

---
+++ notes/express-routing.js
@@ -0,0 +1,60 @@
+// Learning Objective: 
+// - Routing with Express
+
+// INTRODUCTION MATERIAL 
+// Q: What is routing? 
+// - Routing is how an apps or server endpoints (URI's) respond to a clients request
+
+// Q: When does the handler function fire? 
+
+// Q: How many callback functions can a handler function have? 
+
+// ROUTE METHODS
+// Q: Where is the Route Method dervided from? 
+
+// Q: What type of request does Express support? 
+
+// Q: Is there any unique routing method, and if so what is it used for? 
+
+// ROUTE PATHS
+// Q: What is a route path, and what can it be combined with? 
+
+// Q: What characters are subsets of the regualr expression pattern? 
+
+// Q: What if I want to use the $ symbol in my route path? 
+
+// Q: What does express use to match paths? 
+
+// Q: What isn't apart of the route path? 
+
+// ROUTE PARAMATER
+// Q: What are route paramters and what are they used for? 
+
+// Q: How do you define routes with route parameters? 
+
+// Q: How are the hypen(-), and dot(.) interpreted? 
+
+// Q: How can we have more control over an exact string to be used to match a route paramter?
+
+// ROUTE HANDLERS
+// Q: How would I make a callback behave a middleware to handle request? 
+
+// Q: In what forms can route handler be writen in? 
+
+// Q: How would a single callback function handle a route?
+
+// Q: How would more then one callback function handler the route? 
+
+// Q: How would an array of callback functions handle a route? 
+
+// Q: How would a combination of callback functions and arrays be written? 
+
+// RESPONSE METHODS
+// Q: What type of response objects can send a response to the client and terminate the request response cycle? 
+
+// APP.ROUTE()
+// Q: How would I create a chainable route handler for a route path, and why would I want to do that? 
+
+// EXPRESS.ROUTER
+// Q: How would I create, modular, mountable route handlers, and why would I want to do such thing? 
+
---
+++ notes/http-primer.js
@@ -0,0 +1,40 @@
+// Learning Path
+// - HTTP primer for frontend developers
+
+// INTRO
+// Q: What is an HTTP request? What is it used for? 
+// - Hyper Text Protocol or (HTTP) is the system in which the front and backend communicate with one another
+// - Its important to understand how HTTP works becuase a major part of frontend dev is requesting and sending the backend data. AKA sending the server "requests" and handleling "responses"
+
+// REQUEST AND RESPONSE
+// Q: How does communcation work between the client and the server? 
+// - Exchaning messages back and forth
+// - The client (frontend) sends a "request" to the server (backend) sends back a "response" 
+
+// ANATOMY OF AN HTTP REQUEST
+// Q: What are HTTP request composed of? 
+
+// METHOD
+// Q: What are Methods and what are the most common ones?
+
+// URI
+// Q: What is a URI?
+// - Uniform Resource Identifier - This is the path or "location" on the server that identifies the resource that is to be (fetched, created, or modified ...etc)
+
+// REQUEST HEADER 
+// Q: What is a request header, and what is it used for?
+
+// REQUEST BODY 
+// Q: What is the request body, and what is it used for?
+
+// ANATOMY OF AN HTTP REQUEST
+// Q: What are HTTP responses composed of? 
+
+// STATUS
+// Q: What does the status indicate, and why? 
+
+// RESPONSE HEADER
+// Q: What is a response header, and what is it used for?
+
+// RESPONSE BODY
+// Q: What is the response body, and what is it used for?
